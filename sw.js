f1f1a9a45fea203bcfb84395f498e04a6055f34a
terry-eric <67619529+terry-eric@users.noreply.github.com>
2026-05-18 15:22:28 +0800

    fix

---
+++ sw.js
@@ -0,0 +1,9 @@
+const CACHE_NAME = 'sticker-app-v1';
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(['./', './index.html', './manifest.json'])));
+  self.skipWaiting();
+});
+self.addEventListener('activate', (e) => e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch', (e) => {
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+});
